c890812e79cf722397845cf51cabfe9aa77b5093
Private <cheng.john@pm.me>
2021-10-10 22:22:54 +0300

    wip: Add logs about process.env

--- netlify/plugins/netlify-plugin-contextual-env-variables/index.js
+++ netlify/plugins/netlify-plugin-contextual-env-variables/index.js
@@ -2,6 +2,7 @@ module.exports = {
   onPreBuild: () => {
     const context = process.env.CONTEXT.toUpperCase().replace(/-/g, "_");
     console.log(`Updating environment variables with context: ${context}`);
+    console.log(process.env);
     Object.keys(process.env).forEach((key) => {
       const envVar = `${context}_${key}`;
       const val = process.env[envVar];
