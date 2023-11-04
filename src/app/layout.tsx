6e5f8871c099c4476277dfcd0d8e789e053f4c56
John Cheng (john-nhoj) <cheng.john@pm.me>
2023-11-04 16:55:46 +0800

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,39 @@
+import { type Metadata } from 'next'
+
+import { Providers } from '@/app/providers'
+import { Layout } from '@/components/Layout'
+
+import '@/styles/tailwind.css'
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s - John Cheng',
+    default:
+      'John Cheng - Software engineer, founder, and amateur badminton player',
+  },
+  description:
+    'I’m John, a software engineer and entrepreneur currently based in Bali, Indonesia. I’m the founder and CEO of Koodikatu, where we develop technologies that empower regular people to reach their dreams.',
+  alternates: {
+    types: {
+      'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
+    },
+  },
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en" className="h-full antialiased" suppressHydrationWarning>
+      <body className="flex h-full bg-zinc-50 dark:bg-black">
+        <Providers>
+          <div className="flex w-full">
+            <Layout>{children}</Layout>
+          </div>
+        </Providers>
+      </body>
+    </html>
+  )
+}
